5479de8d0cde763a51f14833890f4869ca507703
Paweł Zatoka <github@hoxi.me>
2026-05-24 22:10:05 +0200

    feat: add script to downgrade grub
    needed due to https://bugzilla.redhat.com/show_bug.cgi?id=2263643

---
+++ files/scripts/downgrade-grub.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -oue pipefail
+
+# Your code goes here.
+echo 'Downgrading GRUB...'
+
+mkdir /tmp/downgrade-grub
+cd /tmp/downgrade-grub
+
+dnf5 install -y koji
+koji download-build --arch=x86_64 --arch=noarch grub2-2.12-55.fc44
+dnf5 install -y ./*.rpm --allow-downgrade
+dnf5 remove -y koji
+
+cd /
+rm -rf /tmp/downgrade-grub
